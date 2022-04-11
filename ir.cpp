2d222cb093e4800ff5c95fc065e9c9489ca8ab9b
Heston Liebowitz <hestonl@gmail.com>
2022-04-11 13:44:04 -0600

    Refactor ir module

--- ir.cpp
+++ ir.cpp
@@ -86,15 +86,3 @@ int turnOn() {
 int turnOff() {
     return sendIRCommand(STANDBY_COMMAND);
 }
-
-int main(int argc, char *argv[]) {
-    spdlog::set_level(spdlog::level::debug); // Set global log level to debug
-    spdlog::info("Running tests...");
-
-    char code[255];
-    getIRCode(ON_COMMAND, code);
-    spdlog::info("Test 1: input={} output={}", ON_COMMAND, code);
-
-    getIRCode(STANDBY_COMMAND, code);
-    spdlog::info("Test 2: input={} output={}", STANDBY_COMMAND, code);
-}
